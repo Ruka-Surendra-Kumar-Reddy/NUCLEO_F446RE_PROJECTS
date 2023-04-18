81e914029a443053677620a0985f5ab9974c6d76
Ruka Surendra kumar reddy <127286281+Ruka-Surendra-Kumar-Reddy@users.noreply.github.com>
2023-04-18 10:18:21 +0530

    Add files via upload

---
+++ BASIC_TIMER/Core/Inc/main.h
@@ -0,0 +1,12 @@
+/*
+ * main.h
+ *
+ *  Created on: Dec 26, 2022
+ *      Author: surendra kumar reddy
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+#include"stm32f4xx_hal.h"
+
+#endif /* INC_MAIN_H_ */
